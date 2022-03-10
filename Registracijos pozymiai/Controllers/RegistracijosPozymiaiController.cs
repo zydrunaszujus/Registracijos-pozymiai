b34fbccc4aaac1ff3e9bc8baaa6f86f36cbfada8
zydrunaszujus <89936656+zydrunaszujus@users.noreply.github.com>
2022-03-10 18:37:10 +0200

    Sukurtas projektas.Prideta Models ir Controllers

---
+++ Registracijos pozymiai/Controllers/RegistracijosPozymiaiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Registracijos_pozymiai.Controllers
+{
+    public class RegistracijosPozymiaiController : Controller
+    {
+        // GET: RegistracijosPozymiai
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Registracijos pozymiai/Models/RegistracijosPozymiai.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Registracijos_pozymiai.Models
+{
+    public class RegistracijosPozymiai
+    {
+        public string Pozymiai { get; set; }
+        public string Reiksme { get; set; }
+    }
+}
